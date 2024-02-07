aca82ce398c410d24bc8f3517d754b3a5e7773a4
ALINE BEATRIZ KLUG WILLE <aline_wille@unimedmissoes.coop.br>
2024-02-07 14:07:19 -0300

    configurando notas

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,10 +1,51 @@
-export function NoteCard() {
-    return (
-        
-        <button className='text-left rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none' >
-            <span className='text-sm font-medium text-slate-300'>há 2 dias</span>
-            <p className='text-sm leading-6 text-slate-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga labore sit tenetur porro, harum rem illum quia asperiores nam voluptatibus doloribus corporis itaque autem excepturi mollitia ad eius. Nulla, eius.</p>
-            <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
-        </button>
-    )
+import * as Dialog from "@radix-ui/react-dialog";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import { X } from 'lucide-react'
+ 
+interface NoteCardProps {
+  note: {
+    date: Date;
+    content: string;
+  };
+}
+
+export function NoteCard({ note }: NoteCardProps) {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger className="flex-col text-left rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
+        <span className="text-sm font-medium text-slate-300">
+          {note.date.toISOString()}
+        </span>
+        <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.Content className="overflow-hidden fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] h-[60vh] w-full bg-slate-700 rounded-md flex flex-col outline-none">
+        <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+              <X className="size-5"/>
+        </Dialog.Close>
+          <div className="flex flex-1 flex-col gap-3 p-5">
+            <span className="text-sm font-medium text-slate-300">
+              {formatDistanceToNow(note.date, {
+                locale: ptBR,
+                addSuffix: true,
+              })}
+            </span>
+            <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+          </div>
+          <button
+            type="button"
+            className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-md group">
+            Deseja <span className="text-red-400 group-hover:underline"> apagar essa nota </span>?
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+
+    //<Dialog.Portal> é para o que estiver dentro dele fique na raiz do código, dentro do body e nao dentro da div que está.
+    //Adicionado elemento group ao button para tornar tudo que tem dentro dele parte de um grupo. No elemento filho span adicionado o group antes do hover para que quando passasse o mouse em cima do elemento pai, o hover ativa no elemento filho
+  );
 }
