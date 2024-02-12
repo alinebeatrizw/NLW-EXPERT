a3bce94cdbb14a586216e7fe152647037e491724
Aline Wille <alinewille31@gmail.com>
2024-02-12 12:51:55 -0300

    finalizando envio de notas por texto

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,54 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import logo from "./assets/Logo-nlw-expert.svg";
 import { NewNote } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
 
-
 interface Note {
-  id:string
-  date:Date
-  content: string 
+  id: string;
+  date: Date;
+  content: string;
 }
 
+//sempre que um componente muda, a função de estado roda novamente
+
 export function App() {
+  const [buscar, setBusca] = useState("");
 
-  const [notes,setNotes] = useState<Note[]>([])
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notasSalvas = localStorage.getItem("notes"); //busca no localstorage a key notes
+    if (notasSalvas) {
+      return JSON.parse(notasSalvas);
+    }
+    return [];
+  });
 
-
-  function aoCriarNota(content: string){
+  function aoCriarNota(content: string) {
     const newNote = {
       id: crypto.randomUUID(), //gera um id universal que nao repete nunca
       date: new Date(),
       content,
-    }
+    };
 
     //... pega todas as notas ja criadas
-    const notesArray = [newNote, ...notes]
-    setNotes(notesArray)
+    const notesArray = [newNote, ...notes];
+    setNotes(notesArray);
 
     //transforma em JSON e salva no storage do navegador pois nao é possivel guardar array
-    localStorage.setItem('notes', JSON.stringify(notesArray))
-
+    localStorage.setItem("notes", JSON.stringify(notesArray));
   }
 
+  function deveBuscar(event: ChangeEvent<HTMLInputElement>) {
+    const busca = event.target.value;
+    setBusca(busca);
+  }
 
-  
+  const notaFiltrada =
+    buscar != ""
+      ? notes.filter((note) =>
+          note.content.toLocaleLowerCase().includes(buscar.toLocaleLowerCase())
+        )
+      : notes;
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt="NLW Expert" />
@@ -41,21 +57,21 @@ export function App() {
         <input
           type="text"
           placeholder="Busque em suas notas..."
-          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder: text-slate-500"/>
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder: text-slate-500"
+          onChange={deveBuscar}
+        />
       </form>
 
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        
-        <NewNote aoCriarNota={aoCriarNota}/>
+        <NewNote aoCriarNota={aoCriarNota} />
 
-        {notes.map(note=>{
-          return <NoteCard key={note.id} note={note} />
+        {notaFiltrada.map((note) => {
+          return <NoteCard key={note.id} note={note} />;
         })}
-
       </div>
     </div>
-  )
+  );
 }
-//key é próprio do react. É utilizado para trazer mais performece. 
+//key é próprio do react. É utilizado para trazer mais performece.
