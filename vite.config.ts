f51a09d3607f5ed40da3aa967c82fa4e64486898
Aline Wille <55722252+alinebeatrizw@users.noreply.github.com>
2024-07-16 03:20:02 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/NLW-EXPERT"
+  base: "https://alinebeatrizw.github.io/NLW-Journey-2/"
 })
